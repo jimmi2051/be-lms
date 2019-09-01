16242a542517e64b9352ac4a8d136916054d21d6
Thanh Ly <thanh.ngly@kyanon.digital>
2019-09-01 23:15:38 +0700

    Add middleware show role with creator and user

---
+++ middlewares/listRoles/index.js
@@ -0,0 +1,19 @@
+module.exports = strapi => {
+  return {
+    initialize() {
+      strapi.app.use(async (ctx, next) => {
+        await next();
+        if (ctx.method == "GET" && ctx.url == "/users-permissions/roles") {
+          let tempBody = ctx.response.body.roles;
+          let result = [];
+          tempBody.map((item, index) => {
+            if (item.type === "creator" || item.type === "user") {
+              result.push(item);
+            }
+          });
+          ctx.body = { roles: result };
+        }
+      });
+    }
+  };
+};
