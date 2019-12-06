2bbb454261b3fb95a2f8baad661d071f022c6f50
Deftnt Nguyen <jimmi2051@gmail.com>
2019-12-06 17:37:43 +0700

    [Update] Stream mp4

--- middlewares/listRoles/index.js
+++ middlewares/listRoles/index.js
@@ -1,8 +1,12 @@
+const fs = require('fs')
+const path = require('path')
 module.exports = strapi => {
+  const regexVideo = /\w+\.mp4$/;
   return {
     initialize() {
       strapi.app.use(async (ctx, next) => {
         await next();
+        // Process with api role
         if (
           ctx.method == "GET" &&
           ctx.url == "/users-permissions/roles" &&
@@ -16,6 +20,44 @@ module.exports = strapi => {
             }
           });
           ctx.body.roles = result;
+        }
+        // Process url mp4
+        if (regexVideo.test(ctx.url)) {
+          const path = ctx.url;
+          const stat = fs.statSync(path)
+          const fileSize = stat.size
+          const range = req.headers.range
+          if (range) {
+            const parts = range.replace(/bytes=/, "").split("-")
+            const start = parseInt(parts[0], 10)
+            const end = parts[1]
+              ? parseInt(parts[1], 10)
+              : fileSize - 1
+
+            if (start >= fileSize) {
+              res.status(416).send('Requested range not satisfiable\n' + start + ' >= ' + fileSize);
+              return
+            }
+
+            const chunksize = (end - start) + 1
+            const file = fs.createReadStream(path, { start, end })
+            const head = {
+              'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+              'Accept-Ranges': 'bytes',
+              'Content-Length': chunksize,
+              'Content-Type': 'video/mp4',
+            }
+
+            res.writeHead(206, head)
+            file.pipe(res)
+          } else {
+            const head = {
+              'Content-Length': fileSize,
+              'Content-Type': 'video/mp4',
+            }
+            res.writeHead(200, head)
+            fs.createReadStream(path).pipe(res)
+          }
         }
       });
     }
