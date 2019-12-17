1ca9da8a85247911581e1b244bf668eda27c599e
Thanh Ly <thanh.ngly@kyanon.digital>
2019-12-18 00:04:29 +0700

    [Update] Read file from HTTP Request [Update] Parse csv to array

--- plugins/import/controllers/Import.js
+++ plugins/import/controllers/Import.js
@@ -5,7 +5,9 @@
  *
  * @description: A set of functions called "actions" of the `import` plugin.
  */
-
+const _ = require("lodash");
+const Papa = require("papaparse/papaparse.min.js");
+const fs = require("fs");
 module.exports = {
   /**
    * Default action.
@@ -28,16 +30,25 @@ module.exports = {
     };
   },
   import: async ctx => {
-    const pluginsStore = strapi.store({
-      environment: "",
-      type: "plugin",
-      name: "content-manager"
-    });
-
-    const models = await pluginsStore.get({ key: "schema" });
-
-    ctx.body = {
-      models
-    };
+    const { files = {} } = ctx.request.body.files;
+    if (_.isEmpty(files)) {
+      return ctx.badRequest(
+        null,
+        ctx.request.admin
+          ? [{ messages: [{ id: "Upload.status.empty" }] }]
+          : "Files are empty"
+      );
+    }
+    const content = fs.readFileSync(files.path, "utf8");
+    try {
+      Papa.parse(content, {
+        complete: result => {
+          console.log("result>>>", result.data);
+          ctx.send(result.data);
+        }
+      });
+    } catch {
+      return ctx.badRequest(null, "Format of file is wrong. ");
+    }
   }
 };
